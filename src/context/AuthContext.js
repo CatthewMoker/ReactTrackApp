2e28b96ead211ba5c7e4105e39246078615268c7
Matthew Coker <mattacoker@gmail.com>
2020-12-23 19:41:28 -0500

    beginning to work with local API to get signup to work properly

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,32 +1,31 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return { ...state, errorMessage: action.payload }
         default:
             return state;
-        
     }
 };
 
-const signup = (dispatch) => {
-    return ({ email, password }) => {
-        // make api request to sign up with that email and password
-
-        // if we sign up, modify our state, and say that we are authenticated
-
-        // if signing up fails, we probably ned to reflect an error message somewhere
+const signup = dispatch => {
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password });
+            console.log(response.data);
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up'})
+        }
     };
 };
 
-const signin = (dispatch) => {
-    return ({ email, password }) => {
-        // Try to signin
-        // Handle success by updating state
-        // Handle failure by showing error message
-    };
+const signin = dispatch => {
+    
 };
 
-const signout = (dispatch) => {
+const signout = dispatch => {
     return () => {
         // Sign out
     };
