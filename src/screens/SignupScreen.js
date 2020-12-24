da144d63aeb6807c2c43ef616abcb947c04e3fdd
Matthew Coker <mattacoker@gmail.com>
2020-12-24 12:40:40 -0500

    wired up NavigationRef for navigation outside of React component and refactored out Signup into components that can be reused

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,43 +1,25 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
+import NavLink from '../components/NavLink';
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup} = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+
 
     return ( 
     <View style={styles.container}>
-        <Spacer>
-            <Text h3> Sign up for Tracker</Text>
-        </Spacer>
-        <Spacer>
-            <Input 
-            label="Email"
-            value={email}
-            autoCapitalize="none"
-            onChangeText={setEmail}
-            />
-        </Spacer>
-        <Spacer>
-            <Input 
-            secureTextEntry
-            label="Password"
-            value={password}
-            autoCapitalize="none"
-            onChangeText={setPassword}
-            />
-        </Spacer>
-        {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null }
-        <Spacer>
-            <Button 
-            title="Sign Up" 
-            onPress={() => signup({ email, password})}
-            />
-        </Spacer>
+        <AuthForm 
+        headerText='Sign Up for Tracker'
+        errorMessage={state.errorMessage}
+        submitButtonText='Sign Up'
+        onSubmit={signup}
+        />
+        <NavLink
+            routeName="Signin"
+            linkText="Already have an account? Sign in"
+        />
     </View>
     );
 };
@@ -53,12 +35,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         marginBottom: 250
-    },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15
     }
 });
 
