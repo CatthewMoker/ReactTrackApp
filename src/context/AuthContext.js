e60efd6642f6ec14c008ef8c67c01a3085d5af96
Matthew Coker <mattacoker@gmail.com>
2020-12-25 13:59:00 -0500

    added more to the mapview screen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -6,12 +6,30 @@ import { navigate } from '../navigationRef';
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
-            return { ...state, errorMessage: action.payload }
-        case 'signup':
+            return { ...state, errorMessage: action.payload };
+        case 'signin':
             return { errorMessage: '', token: action.payload };
+        case 'signout':
+            return { token:null, errorMessage:null };
+        case 'clear_error_message':
+            return { ...state, errorMessage: '' };
         default:
             return state;
     }
+};
+
+const tryLocalSignin = dispatch => async () => {
+    const token = await AsyncStorage.getItem('token');
+    if (token) {
+        dispatch({ type: 'signin', payload: token });
+        navigate('TrackList');
+    } else {
+        navigate('loginFlow');
+    }
+};
+
+const clearErrorMessage = dispatch => () => {
+    dispatch({ type: 'clear_error_message' })
 };
 
 const signup = dispatch => {
@@ -19,7 +37,7 @@ const signup = dispatch => {
         try {
             const response = await trackerApi.post('/signup', { email, password });
             await AsyncStorage.setItem('token', response.data.token);
-            dispatch({ type: 'singup', payload: response.data.token })
+            dispatch({ type: 'signin', payload: response.data.token })
 
             navigate('TrackList');
         } catch (err) {
@@ -28,18 +46,26 @@ const signup = dispatch => {
     };
 };
 
-const signin = dispatch => {
-    
-};
-
-const signout = dispatch => {
-    return () => {
-        // Sign out
+const signin = dispatch => async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signin', { email, password });
+            await AsyncStorage.setItem('token', response.data.token);
+            dispatch({ type: 'signin', payload: response.data.token });
+            navigate('TrackList')
+        } catch (err) {
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign in'});
+        }
     };
-};
+
+const signout = dispatch => async () => {
+       await AsyncStorage.removeItem('token');
+       dispatch({ type: 'signout' });
+       navigate('loginFlow');
+    };
+
 
 export const { Provider, Context } = createDataContext(
     authReducer,
-    { signup, signin, signout },
+    { signup, signin, signout, clearErrorMessage, tryLocalSignin },
     { token: null, errorMessage: '' }
 );
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Text } from 'react-native-elements';
+import { SafeAreaView } from 'react-navigation';
+import Map from '../components/Map';
 
 const TaskCreateScreen = () => {
-    return <Text style={{ fontSize:48 }}>TaskCreateScreen</Text>;
+    return (
+        <SafeAreaView forceInset={{ top: 'always' }}>
+            <Text h2>Create a track</Text>
+            <Map/>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({});
