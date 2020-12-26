e0c3f1234822b4b58a4424fe4a6b77292548b1b7
Matthew Coker <mattacoker@gmail.com>
2020-12-26 14:59:08 -0500

    Set up location context and automated movement

--- App.js
+++ App.js
@@ -12,6 +12,7 @@ import TrackDetailScreen from './src/screens/TrackDetailScreen';
 import TrackListScreen from './src/screens/TrackListScreen';
 
 import { Provider as AuthProvider } from './src/context/AuthContext';
+import { Provider as LocationProvider } from './src/context/LocationContext';
 import { setNavigator } from './src/navigationRef';
 import ResolveAuthScreen from './src/screens/ResolveAuthScreen';
 
@@ -34,7 +35,11 @@ const switchNavigator = createSwitchNavigator({
 const App = createAppContainer(switchNavigator);
 
 export default () => {
-  return <AuthProvider>
-    <App ref={(navigator) => { setNavigator(navigator) }} />
-  </AuthProvider>
+  return ( 
+  <LocationProvider>
+    <AuthProvider>
+      <App ref={(navigator) => { setNavigator(navigator) }} />
+    </AuthProvider>
+  </LocationProvider>
+  )
 }
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import '../_mockLocation';
+
+import React, { useEffect, useState, useContext } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
+import { requestPermissionsAsync, watchPositionAsync, Accuracy } from 'expo-location';
 import Map from '../components/Map';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TaskCreateScreen = () => {
+    const { addLocation } = useContext(LocationContext);
+    const [err, setErr] = useState(null);
+
+    const startWatching = async () => {
+        try {
+            const { granted } = await requestPermissionsAsync();
+            await watchPositionAsync({
+                accuracy: Accuracy.BestForNavigation,
+                timeInterval: 1000,
+                distanceInterval: 10
+            }, (location) => {
+                addLocation(location);
+            });
+            if (!granted) {
+                throw new Error('Location permission not granted');
+            }
+        } catch (e) {
+            setErr(e);
+        }
+    };
+
+    useEffect(() => {
+        startWatching();
+    }, []);
+
     return (
         <SafeAreaView forceInset={{ top: 'always' }}>
             <Text h2>Create a track</Text>
             <Map/>
+            {err ? <Text>Please enable location services</Text>:null}
         </SafeAreaView>
     );
 };
