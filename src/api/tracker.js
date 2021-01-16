bbe0653dd91dcee04bb9c93e470f107690c74679
Matthew Coker <mattacoker@gmail.com>
2021-01-15 21:03:55 -0500

    abstracted the environment variables to app.config.js

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,21 +1,22 @@
-import axios from 'axios';
-import { AsyncStorage } from 'react-native';
+import axios from "axios";
+import { AsyncStorage } from "react-native";
+import Constants from "expo-constants";
 
 const instance = axios.create({
-     baseURL: ""
+  baseURL: Constants.manifest.baseURL,
 });
 
 instance.interceptors.request.use(
-     async (config) => {
-          const token = await AsyncStorage.getItem('token');
-          if (token) {
-               config.headers.Authorization = `Bearer ${token}`;
-          }
-          return config;
-     },
-     (err) => {
-          return Promise.reject(err);
-     }
+  async (config) => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  }
 );
 
 export default instance;
